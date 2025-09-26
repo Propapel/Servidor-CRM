1a49d30d29c3f816d2211b28e777e548465e0f37
egasparm <eliasmenapech@gmail.com>
2025-09-26 12:37:04 -0600

    Fix problem with limit body

--- src/main.ts
+++ src/main.ts
@@ -6,10 +6,11 @@ import { ValidationPipe } from '@nestjs/common';
 import * as bodyParser from 'body-parser'; 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
   app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: false }));
   // Aumentar el límite de tamaño del cuerpo de la solicitud
-  app.use(bodyParser.json({ limit: '1000mb' })); // Cambia '10mb' según sea necesario
-  app.use(bodyParser.urlencoded({ limit: '1000mb', extended: true }));
+  app.use(bodyParser.json({ limit: '100mb' })); // Cambia '10mb' según sea necesario
+  app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
   // Use environment variable for the host, defaulting to 'localhost'
   const host = process.env.HOST || 'localhost';
   const port = process.env.PORT || 3002;

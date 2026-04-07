2cc632fc420f311fff7166943a60b2708dffcd21
eliasdev <eliasmenapech@gmail.com>
2026-04-07 09:50:15 -0600

    Config the env port for the database

--- src/app.module.ts
+++ src/app.module.ts
@@ -55,7 +55,7 @@ import { EquipmentReplacementModule } from './equipment-replacement/equipment-re
     TypeOrmModule.forRoot({
       type: 'mysql',
       host: process.env.HOST_DB,
-      port: 3306,
+      port: parseInt(process.env.DB_PORT, 10) || 3306,
       username: process.env.DB_USER,
       password: process.env.DB_PASSWORD,
       database: process.env.DB,

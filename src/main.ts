cf413466a8f814c730385e9cc17e4899fc5a17df
eliasdev <eliasmenapech@gmail.com>
2026-04-23 11:14:50 -0600

    fdsfd

--- src/main.ts
+++ src/main.ts
@@ -3,26 +3,16 @@ process.env.TZ = 'UTC';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
-import { ExpressAdapter } from '@nestjs/platform-express';
-import serverlessExpress from '@vendia/serverless-express';
+import { WsAdapter } from '@nestjs/platform-ws';
 import * as bodyParser from 'body-parser';
-import express from 'express';
-
-const expressApp = express();
-let cachedServer: any; // ✅ Simplificado para evitar error de tipado
 
 async function bootstrap() {
-  const app = await NestFactory.create(
-    AppModule,
-    new ExpressAdapter(expressApp),
-    { bufferLogs: true }
-  );
-
+  const app = await NestFactory.create(AppModule);
   app.enableCors({
     origin: [
       'https://www.mesadeayudasaimid.org',
       'https://mesadeayudasaimid.org',
-      'https://propapel.vercel.app',
+      'https://propapel.vercel.app'
     ],
     methods: 'GET,POST,PUT,DELETE,PATCH,OPTIONS',
     allowedHeaders: 'Content-Type, Authorization, x-api-key',
@@ -31,15 +21,11 @@ async function bootstrap() {
   app.useGlobalPipes(new ValidationPipe({ forbidUnknownValues: false }));
   app.use(bodyParser.json({ limit: '100mb' }));
   app.use(bodyParser.urlencoded({ limit: '100mb', extended: true }));
+  app.useWebSocketAdapter(new WsAdapter(app));
 
-  await app.init();
+  const host = process.env.HOST || 'localhost';
+  const port = process.env.PORT || 3002;
 
-  return serverlessExpress({ app: expressApp }); // ✅ Retorna el handler
+  await app.listen(port, host);
 }
-
-module.exports = async (req: any, res: any) => {
-  if (!cachedServer) {
-    cachedServer = await bootstrap();
-  }
-  return cachedServer(req, res);
-};
+bootstrap();
